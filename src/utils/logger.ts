8cc6076e75358cee3cf1a819907a868ed6f99ced
AmirReza <AmirReza@gmail.com>
2025-10-13 14:43:09 +0330

    add:logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,41 @@
+// src/common/logger.ts
+import { createLogger, format, transports } from 'winston';
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
+import * as path from 'path';
+
+const logDir = path.join(process.cwd(), 'logs');
+
+const logger = createLogger({
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.errors({ stack: true }),
+        format.printf(({ level, message, timestamp, stack }) => {
+            return `[${timestamp}] ${level.toUpperCase()}: ${message}${stack ? '\n' + stack : ''}`;
+        }),
+    ),
+    transports: [
+        // نمایش در ترمینال
+        new transports.Console({
+            format: format.combine(
+                format.colorize(),
+                nestWinstonModuleUtilities.format.nestLike('HayaApp', {
+                    prettyPrint: true,
+                }),
+            ),
+        }),
+
+        // ذخیره لاگ‌ها در فایل
+        new transports.File({
+            filename: path.join(logDir, 'app.log'),
+            level: 'info',
+        }),
+
+        new transports.File({
+            filename: path.join(logDir, 'errors.log'),
+            level: 'error',
+        }),
+    ],
+});
+
+export default logger;
