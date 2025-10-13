ed36413ea7c4aac4039c4e01592ccc7de6f05515
AmirReza <AmirReza@gmail.com>
2025-10-13 15:01:03 +0330

    add:.env.test

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import logger from 'src/utils/logger';
+import logger from '../utils/logger';
 
 @Injectable()
 export class PrismaService
