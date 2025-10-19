bfb1b88345ff72fb714e8321cb22eeca8f3b9519
AmirReza <AmirReza@gmail.com>
2025-10-19 10:12:23 +0330

    resolve gitignor

--- src/assessment/entities/assessment.entity.ts
+++ src/assessment/entities/assessment.entity.ts
@@ -12,6 +12,7 @@ export class Assessment {
   map_points?: { lat: number; lng: number }[];
   status: Status;
   reference_code?: string;
+  created_by: number;
   createdAt: Date;
   updatedAt: Date;
   deletedAt?: Date;
