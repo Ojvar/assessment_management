aa773bf8c4555efc174c23b7b3f2f23d4960e094
AmirReza <AmirReza@gmail.com>
2025-10-19 10:17:35 +0330

    finish rebase

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,6 @@
 import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
 import * as path from 'path';
-<<<<<<< HEAD
-import { createLogger, format, transports } from 'winston';
-=======
 import { createLogger, format, Logform, transports } from 'winston';
->>>>>>> d443680 (Fix linter errors)
 
 const logDir = path.join(process.cwd(), 'logs');
 
@@ -22,27 +18,6 @@ const logger = createLogger({
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     format.errors({ stack: true }),
-<<<<<<< HEAD
-    format.printf(({ level, message, timestamp, stack }) => {
-      return `[${timestamp}] ${level.toUpperCase()}: ${message}${stack ? '\n' + stack : ''}`;
-    }),
-  ),
-  transports: [
-    new transports.Console({
-      format: format.combine(
-        format.colorize(),
-        nestWinstonModuleUtilities.format.nestLike('HayaApp', {
-          prettyPrint: true,
-        }),
-      ),
-    }),
-
-    new transports.File({
-      filename: path.join(logDir, 'app.log'),
-      level: 'info',
-    }),
-
-=======
     format.printf((info: Logform.TransformableInfo) => {
       const message = safeString(info.message);
 
@@ -72,7 +47,6 @@ const logger = createLogger({
       filename: path.join(logDir, 'app.log'),
       level: 'info',
     }),
->>>>>>> d443680 (Fix linter errors)
     new transports.File({
       filename: path.join(logDir, 'errors.log'),
       level: 'error',
