102f087e73da7e7fb0e9dd7224e7631986af2c60
ojvar <mojvar.amirhossein@gmail.com>
2025-10-25 09:15:32 +0330

    clean up project
    setup prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AssessmentModule } from './assessment/assessment.module';
-import { AuthModule } from './auth/auth.module';
-import { UserController } from './users/user.controller';
-import { UsersModule } from './users/user.module';
-import { UsersService } from './users/user.service';
 
 @Module({
-  imports: [AssessmentModule, AuthModule, UsersModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UsersService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
