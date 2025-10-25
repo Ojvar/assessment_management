359b0818a5bb105d86fdf0ba024972521a3a6e2f
ojvar <mojvar.amirhossein@gmail.com>
2025-10-25 09:41:24 +0330

    update: users resource

---
+++ src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, HttpCode, Param } from '@nestjs/common';
+import { User } from 'generated/prisma';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('/:id')
+  @HttpCode(200)
+  findByEmail(@Param('id') id: string): Promise<User> {
+    return this.usersService.findById(id);
+  }
+}
