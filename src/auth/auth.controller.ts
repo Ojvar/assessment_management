e5f5dd000931b31495b7394c48fece681a93ad44
AmirReza <AmirReza@gmail.com>
2025-10-25 15:20:40 +0330

    add guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDTO } from './dto/login.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { JwtPayload } from './types';
 
 @Controller('auth')
@@ -9,10 +11,9 @@ export class AuthController {
 
   @Post('/login')
   @HttpCode(200)
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   login(@Body() loginDto: LoginDTO): Promise<JwtPayload> {
-    return this.authService.validateUser(
-      loginDto.email,
-      loginDto.password,
-    );
+    return this.authService.validateUser(loginDto.email, loginDto.password);
   }
 }
