cbfa762dcb83dbf049b005a491ec14c511a07c56
AmirReza <AmirReza@gmail.com>
2025-11-01 12:45:25 +0330

    add property to user dto

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UserDTO {
+  @ApiProperty()
   id: number;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   password: string;
 }
