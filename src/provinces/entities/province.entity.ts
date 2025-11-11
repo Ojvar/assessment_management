a35677d620b83c3d42c560c9180467d8f5be5ccc
AmirReza <AmirReza@gmail.com>
2025-11-11 15:26:46 +0330

    fix entity error

--- src/provinces/entities/province.entity.ts
+++ src/provinces/entities/province.entity.ts
@@ -1,16 +1,1 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity('provinces')
-export class Province {
-    @PrimaryGeneratedColumn()
-    id: number;
-
-    @Column({ type: 'varchar', length: 255 })
-    name: string;
-
-    constructor(data?: Partial<Province>) {
-        if (data) {
-            Object.assign(this, data);
-        }
-    }
-}
+export class Province { }
